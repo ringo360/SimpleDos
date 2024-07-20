4b1bff9ae0e673c92043cee12432adf4de28c52d
ringoXD <ringoxd@ringoxd.dev>
2024-07-21 02:48:11 +0900

    send()

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,30 @@ import color from 'picocolors';
 
 const version = '1.0.0';
 
+const threads = 100;
+
 consola.info(`QuickSpammer v${version}`);
 
 async function inputURL() {
-	const res = await consola.prompt('Target URL (ex: https://google.com/)');
-	if ('') {
+	const res = (await consola.prompt('Target URL (ex: https://google.com/)')) as string;
+	consola.info('Checking...');
+	if (!res || !res.startsWith('http')) {
+		consola.error('Invalid input.');
+		return inputURL();
+	}
+	return res;
+}
+
+async function send() {
+	try {
+		//todo: fetch
+	} catch (e) {
+		consola.log(e);
 	}
 }
 
 async function main() {
-	const url = await consola.prompt('Target URL');
+	const url = await inputURL();
 }
 
 main();
